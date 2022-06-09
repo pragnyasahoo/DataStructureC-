13a3ffa0e8257047bc5cfc9815a081cc54b83d59
Pragnya Sahoo <pragnya.pradip@gmail.com>
2022-06-09 15:58:54 +0530

    Find the Repeated word in Sentence

--- Program.cs
+++ Program.cs
@@ -7,29 +7,10 @@ namespace datastructure
 {
     class Program
     {
-            static void Main(string[] args)
+       static void Main(string[] args)
         { 
+             
 
-            Pallendron pallendron = new Pallendron();
-            arraycode arraycode = new arraycode();
-            //arraycode.firstRepeatedoccureanceIndex();
-            // arraycode.MaximumProductSubarray(); 
-            // arraycode.repeatWordInSentence("I am good I am good good");
-            // arraycode.removeDuplicateintValue();
-            // arraycode.removeDuplicateStringValue();
-
-            //new
-            //pallendron. palindromepair();
-            //pallendron.reverseName("pragnya SAhoo");\
-
-          
-
-            /* Common c = new Common();
-             c.swap();*/
-            MatrixAdd matrixadd = new MatrixAdd();
-
-            matrixadd.load(); 
-
-            }  
+        }  
     }
 }
---
+++ StringOperation/RepeatWordInSentence.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace datastructure.StringOperation
+{
+    internal class RepeatWordInSentence
+    {
+        public void repeatWordInSentence(string sentence)
+        {
+            string[] word = sentence.Split(' ');
+            int i, j, count = 1;
+            List<string> words = new List<string>();
+
+
+            for (i = 0; i < word.Length - 1; i++)
+            {
+                for (j = i + 1; j < word.Length; j++)
+                {
+                    if (word[i] == word[j] && word[i] !=null && word[j]!=null)
+                    {
+                        word[j] = null;
+                        count++; 
+                    } 
+                }
+
+                if (count > 1)
+                    words.Add(word[i] + ":" + count);
+                count = 1;
+            }
+
+            for (int k = 0; k < words.Count; k++)
+            {
+                Console.WriteLine("{0}", words[k]);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            RepeatWordInSentence repeatWordInSentence = new RepeatWordInSentence();
+            repeatWordInSentence.repeatWordInSentence("I am not gonna live forever, but I wanna live while I am alive");
+        }
+    }
+}
